4c86bc8a3cfd780c43d1df37148c38a2ed13ffed
MarceloGoc <marcelogochez@gmail.com>
2024-03-06 21:44:48 -0300

    primer commit

---
+++ codigo.js
@@ -0,0 +1,58 @@
+let continuar = true
+let nombre = prompt("ingrese su nombre");
+let empresa = prompt("ingrese la empresa");
+let fecha = prompt("ingrese la fecha");
+let totalActividad = [];
+let precioActividad = [];
+let precioTotal = precioActividad.reduce((acumulador, precio) => acumulador + precio, 0);
+let resultado = {
+    nombre: nombre,
+    empresa: empresa,
+    fecha: fecha,
+    totalActividad: totalActividad,
+    precioActividad: precioActividad,
+    precioTotal: precioTotal
+   };
+
+
+while (continuar) {
+
+    let actividades = parseInt(prompt("ingrese actividad <br> 1 Acometida, 2 Boca catv, 3 Deco HD, 4 Modem WiFi, 5 salir"));
+    switch (actividades){
+        case 1:
+        totalActividad.push("Acometida")
+        precioActividad.push(1000)
+        break
+
+    case 2:
+        totalActividad.push("Boca catv")
+        precioActividad.push(700)
+        break
+
+        case 3:
+        totalActividad.push("Deco HD")
+        precioActividad.push(2500)
+        break
+
+        case 4: 
+        totalActividad.push("Modem WiFi")
+        precioActividad.push(3000)
+        break
+
+        default:
+         console.log("Opción inválida")
+    }
+
+    let confirmacion = prompt("Desea agregar otra actividad? (si/no)").toLowerCase()
+    if (confirmacion == "no") {
+        continuar = false
+        console.log(resultado)
+    }
+}
+
+
+
+
+
+
+
